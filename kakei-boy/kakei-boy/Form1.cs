6f2b813b74c200bfa492f6ff29b1503971eca277
honya8bit <honya.no.byte@gmail.com>
2014-06-16 23:35:27 +0900

    KB-ver0.1.1
    valueの読み込みまで

--- kakei-boy/kakei-boy/Form1.cs
+++ kakei-boy/kakei-boy/Form1.cs
@@ -43,16 +43,27 @@ namespace kakei_boy
             {
                 MySqlConnection conn = new MySqlConnection();
                 conn.ConnectionString = "Host=localhost;user id=kamimura;password=kamimura;database=kakeiboy";
-                MySqlCommand com = new MySqlCommand("insert into test values(1145148101919);",conn);
-                com.Connection.Open();
-                com.ExecuteNonQuery();
-                int i = 0;
+                MySqlCommand com = new MySqlCommand("select * from data;", conn);
+                conn.Open();
+                MySqlDataReader reader = com.ExecuteReader();
+
+                string[] names = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                    names[i] = reader.GetName(i);
+
+                // readでレコードを進めてvalueカラムを読む
+                // メモ：readerはカーソル
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader.GetInt32("value").ToString());
+                }
+
             }
             catch (Exception e)
             {
+                MessageBox.Show(e.ToString());
                 ;
             }
-            /* チャートの更新 */
             
             
         }
